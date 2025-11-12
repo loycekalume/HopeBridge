49bb83b1860fc6d401393d4289fe5d991d42f265
Loyce <loyce.kalume22@students.dkut.ac.ke>
2025-11-12 23:38:07 +0300

    rectified scripts commands to build dist

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -32,12 +32,27 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    methods: "POST,GET, PUT,PATCH,DELETE",
-    credentials: true
-}))
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+   process.env.FRONTEND_URL,
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //4. routes 
 app.use("/api/auth", authRoutes),
